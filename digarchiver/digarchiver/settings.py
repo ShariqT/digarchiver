f7aa0faeefa52c0838a89cdf2673e482ac18f1f7
Shariq Torres <shariq.torres@gmail.com>
2016-07-28 10:49:38 -0400

    added some more stuff for templates

--- digarchiver/digarchiver/settings.py
+++ digarchiver/digarchiver/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'doi.apps.DoiConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
--- digarchiver/doi/urls.py
+++ digarchiver/doi/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name="index"),
+	url(r'^process$', views.process, name="process"),
+	url(r'^details/(?P<project_id>[0-9]+)$', views.details, name='details'),
+	url(r'^processdetails$', views.processdetails, name="processdetails"),
+	url(r'^share$', views.share, name="share"),
 ]
